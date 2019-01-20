d07c5241e6a2a67519910586a6e3af281f19652e
MJ <mjackson74u@gmail.com>
2019-01-20 18:52:48 -0500

    test/[#11] - Added Unit test for decrement inventory

--- src/test/java/com/mycompany/app/modelTests/ProductTest.java
+++ src/test/java/com/mycompany/app/modelTests/ProductTest.java
@@ -1,14 +1,21 @@
 package com.mycompany.app.modelTests;
 
+import com.mycompany.app.exception.InventoryEmptyException;
 import com.mycompany.app.model.Product;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ProductTest {
+    Product product;
+
+    @Before
+    public void setup() {
+        product = new Product(1, "Test Product", 5.99, 1);
+    }
 
     @Test
     public void t001_inventoryEmptyTest() {
-        Product product = new Product(1, "Test Product", 5.99, 1);
         assertFalse("Inventory Count is greater than 0", product.inventoryEmpty());
 
         product.setInventory(10);
@@ -16,5 +23,19 @@ public class ProductTest {
 
         product.setInventory(0);
         assertTrue("Inventory Count is 0", product.inventoryEmpty());
+    }
+
+    @Test
+    public void t002_decrementInventoryTest() {
+        product.decrementInventory();
+
+        assertEquals("Did not decrement inventory", 0, product.getInventory());
+
+        try {
+            product.decrementInventory();
+            fail("Did not properly throw exception when attempting to decrement inventory");
+        } catch (InventoryEmptyException e) {
+
+        }
     }
 }
